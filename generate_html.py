b3b1b6181d45e6b934e343dc4ec788f5ba233b19
Matthew <matthew.marinets@gmail.com>
2024-01-10 00:47:59 -0800

    Updated styles and formatting on HTML page

--- generate_html.py
+++ generate_html.py
@@ -18,18 +18,30 @@ def write_start(fp: io.FileIO) -> None:
         <div id="main-content">
     """))
 
+def brief_name(item_name: str) -> str:
+    return item_name.split('(', 1)[0].strip().replace(' ', '-')
+
+def write_table_of_contents(fp: io.FileIO, item_names: Iterable[str]) -> None:
+    fp.write("""
+    <div id="toc">
+    <h2>Table of Contents</h2>
+        <ol>
+    """)
+    for item_name in item_names:
+        fp.write(f'<li><a href="#{brief_name(item_name)}">{item_name}</a></li>\n')
+    fp.write('</ol></div>\n')
+
 def write_item(fp: io.FileIO, item_name: str, item_info: str, icon_location: str|None) -> None:
-    item_name_brief = item_name.split('(', 1)[0].strip().replace(' ', '-')
     fp.write(inspect.cleandoc(f"""
     <div id="{item_name}">
-        <h3><a id="{item_name_brief}"></a>{item_name}</h3>
+        <h2><a id="{brief_name(item_name)}"></a>{item_name}</h2>
         {f'<img src="{icon_location}"/>' if icon_location else '<p class="error">Icon currently unavailable</p>'}
         <ul>
         <li>Faction: {item_info["race"]}</li>
         <li>Classification: {item_info["classification"]}</li>
         {f'<li>Description: {item_info["description"]}</li>' if item_info["description"] else ''}
         {f'<li>Parent: {item_info["parent_item"]}</li>' if item_info["parent_item"] else ''}
-        {f'<li>Icon path: {icon_location}</li>' if icon_location else ''}
+        {f'<li>Icon path: <code>{icon_location}</code></li>' if icon_location else ''}
         </ul>
     </div>
     """))
@@ -48,6 +60,7 @@ if __name__ == '__main__':
         icon_manifest = json.load(fp)
     with open('index.html', 'w') as fp:
         write_start(fp)
+        write_table_of_contents(fp, item_data)
         for item in item_data:
             write_item(fp, item, item_data[item], icon_manifest.get(item))
         write_end(fp)
