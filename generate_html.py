9eb4676400d0b5df6d95c9725c8ca175af9bcaa6
Matthew <matthew.marinets@gmail.com>
2024-01-18 12:15:56 -0800

    Updated with latest new icons; updated overrides; icon headings are now links; sorted ToC

--- generate_html.py
+++ generate_html.py
@@ -10,8 +10,8 @@ def write_start(fp: io.FileIO) -> None:
     <html>
     <head>
         <title>Starcraft 2 Icon Repository</title>
-        <meta name="description" content="A repository of Starcraft 2 icons used in Archipelago">
-        <meta name="keywords" content="Archipelago Starcraft 2">
+        <meta name="description" content="A repository of Starcraft 2 icons used in Archipelago"/>
+        <meta name="keywords" content="Archipelago Starcraft 2"/>
         <link rel="stylesheet" href="styles/common.css"/>
     </head>
     <body style="background-color: black; color: #ebb">
@@ -27,14 +27,14 @@ def write_table_of_contents(fp: io.FileIO, item_names: Iterable[str]) -> None:
     <h2>Table of Contents</h2>
     <ol>
     """)
-    for item_name in item_names:
+    for item_name in sorted(item_names, key=lambda x: tuple(reversed(x.split(' ('))) if ' (' in x else ('', x)):
         fp.write(f'<li><a href="#{brief_name(item_name)}">{item_name}</a></li>\n')
     fp.write('</ol></div>\n')
 
 def write_item(fp: io.FileIO, item_name: str, item_info: str, icon_locations: list[str]) -> None:
     fp.write(inspect.cleandoc(f"""
     <div id="{item_name}">
-        <h2><a id="{brief_name(item_name)}"></a>{item_name}</h2>
+        <a id="{brief_name(item_name)}"></a><a href="#{brief_name(item_name)}"><h2> {item_name}</h2></a>
         <div class="image-container">
     """))
     if not icon_locations:
